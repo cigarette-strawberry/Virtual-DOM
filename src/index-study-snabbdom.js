f147070b6433b41c23508bf7e27d06515b53d734
chenwu9 <chenwu9@flemply.com>
2025-12-25 16:27:37 +0800

    feat:感受diff算法

--- src/index-study-snabbdom.js
+++ src/index-study-snabbdom.js
@@ -1,33 +1,19 @@
-import {
-  init,
-  classModule,
-  propsModule,
-  styleModule,
-  eventListenersModule,
-  h,
-} from "snabbdom";
+import h from "./my-snabbdom/h.js";
 
-const patch = init([
-  // 通过传入模块初始化 patch 函数
-  classModule, // 开启 classes 功能
-  propsModule, // 支持传入 props
-  styleModule, // 支持内联样式同时支持动画
-  eventListenersModule, // 添加事件监听
+/* h("div", {}, "");
+h("div", {}, []);
+h("div", {}, h()); */
+
+const vnode1 = h("div", {}, "hello");
+console.log("🚀 ~ vnode1:", vnode1);
+
+const vnode2 = h("div", {}, [
+  h("p", {}, "hello1"),
+  h("p", {}, "hello2"),
+  h("p", {}, "hello3"),
+  h("p", {}, h("span", {}, "hello4")),
 ]);
+console.log("🚀 ~ vnode2:", vnode2);
 
-const container = document.getElementById("container");
-
-// 创建虚拟节点
-const vNode = h("div", { style: { color: "red" } }, "这是一个红色的div");
-console.log("🚀 ~ vNode:", vNode);
-
-// 嵌套h函数
-const vNode1 = h("ul", { style: { color: "red" } }, [
-  h("li", {}, "这是一个红色的div"),
-  h("li", {}, [h("div", {}, "这是一个红色的div嵌套")]),
-  h("li", {}, "这是一个红色的div"),
-]);
-console.log("🚀 ~ vNode1:", vNode1);
-
-// 让虚拟节点上树
-patch(container, vNode1);
+const vnode3 = h("div", {}, h("p", {}, "hello"));
+console.log("🚀 ~ vnode3:", vnode3);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
-import h from "./my-snabbdom/h.js";
+import {
+  init,
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule,
+  h,
+} from "snabbdom";
 
-/* h("div", {}, "");
-h("div", {}, []);
-h("div", {}, h()); */
-
-const vnode1 = h("div", {}, "hello");
-console.log("🚀 ~ vnode1:", vnode1);
-
-const vnode2 = h("div", {}, [
-  h("p", {}, "hello1"),
-  h("p", {}, "hello2"),
-  h("p", {}, "hello3"),
-  h("p", {}, h("span", {}, "hello4")),
+const patch = init([
+  // 通过传入模块初始化 patch 函数
+  classModule, // 开启 classes 功能
+  propsModule, // 支持传入 props
+  styleModule, // 支持内联样式同时支持动画
+  eventListenersModule, // 添加事件监听
 ]);
-console.log("🚀 ~ vnode2:", vnode2);
 
-const vnode3 = h("div", {}, h("p", {}, "hello"));
-console.log("🚀 ~ vnode3:", vnode3);
+const container = document.getElementById("container");
+
+// 创建虚拟节点
+const vNode = h("div", { style: { color: "red" } }, "这是一个红色的div");
+console.log("🚀 ~ vNode:", vNode);
+
+// 嵌套h函数
+const vNode1 = h("ul", { style: { color: "red" } }, [
+  h("li", { key: "a" }, "这是一个红色的div"),
+  h("li", { key: "b" }, [h("div", {}, "这是一个红色的div嵌套")]),
+  h("li", { key: "c" }, "这是一个红色的div"),
+]);
+console.log("🚀 ~ vNode1:", vNode1);
+
+// 让虚拟节点上树
+patch(container, vNode1);
+
+const vNode2 = h("ol", { style: { color: "red" } }, [
+  h("li", { key: "a" }, "这是一个红色的div"),
+  h("li", { key: "b" }, [h("div", {}, "这是一个红色的div嵌套")]),
+  h("li", { key: "c" }, "这是一个红色的div"),
+  h("li", { key: "d" }, "这是一个红色的div11111111111"),
+]);
+
+// 让虚拟节点上树
+patch(vNode1, vNode2);
+
+/* setInterval(() => {
+  // 让虚拟节点上树
+  patch(vNode1, vNode2);
+}, 1000); */
